733a0b22368b81a443c2395b1f594f6f55f56041
dongstrans <131533486+dongstrans@users.noreply.github.com>
2023-04-22 14:44:28 +0100

    Testing file upload app
    Testing file upload app

---
+++ test.py
@@ -0,0 +1,10 @@
+# Import python libraries
+import matplotlib.pyplot as plt; plt.rcdefaults()
+%matplotlib inline
+import numpy as np
+import pandas as pd
+
+# Read csv file
+df = pd.read_csv('london.csv')
+
+df
