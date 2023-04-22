f7bc83cd79bd5475ca15e30e04a17698bfa7788c
dongstrans <131533486+dongstrans@users.noreply.github.com>
2023-04-22 15:02:25 +0100

    imported sklearn
    imported sklearn

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 # Import python libraries
 import streamlit as st
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 #%matplotlib inline
 import numpy as np
 import pandas as pd
+from sklearn import preprocessing
 
 # Read csv file
 df = pd.read_csv('london.csv')
