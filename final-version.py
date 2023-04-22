9f0bf2f71df97c995c81714912c31f5086cc44ec
dongstrans <131533486+dongstrans@users.noreply.github.com>
2023-04-22 18:01:12 +0100

    Add files via upload

--- final-version.py
+++ final-version.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
+import glob
 
 st.write("""
 # Stop Search Crime Prediction
@@ -35,9 +36,38 @@ borough = df['Borough'].unique()
 borough
 print(borough)
 
-st.write("""
-![alt text](/assets/images/Barking and Dagenham.PNG)
-""")
+def load_images():
+    image_files = glob.glob(*.png)
+    manuscripts = []
+    for image_file in image_files:
+        image_file = image_file.replace("\\", "/")
+        parts = image_file.split("/")
+        if parts[1] not in manuscripts:
+            manuscripts.append(parts[1])
+        manuscripts.sort()
+    
+    return image_files, manuscripts
+
+st.title("Images of graphs")
+image_files, manuscripts = load_images()
+view_manuscripts = st.multiselect("Select Manuscript(s)", manuscripts)
+n = st.number_input("Select Grid Width", 1, 5, 3)
+
+view_images = []
+for image_file in image_files:
+    if any(manuscript in image_file for manuscript
+    in view_manuscripts):
+        st.write(image_file)
+            view_images.append(image_file)
+            
+groups = []
+for i in range(0, len(view_images), n):
+    groups.append(view_images[i:i+n])
+    
+for group in groups:
+    cols = st.columns(n)
+    for i, image_file in enumerate(group):
+        cols[i].image(image_file)
 
 # Plot graph
 for area in borough:
